925bf8b8751508612b20c19a41b068ebb9b201fa
Seren Kwok <86184664+seren5240@users.noreply.github.com>
2023-03-27 13:37:30 -0600

    Update another.js

--- another.js
+++ another.js
@@ -1,4 +1,4 @@
 console.error("add a few more lines so it shows as modified");
 console.error("fun");
 console.error("yet again");
-console.log('change')
+console.log('how do you solve a problem like')
