1c52f73ce0568ec2226ae486a7df5fbd66248f5f
Seren Kwok <86184664+seren5240@users.noreply.github.com>
2023-08-10 12:36:52 -0400

    Update another.ts

--- another.ts
+++ another.ts
@@ -1,5 +1,5 @@
 console.error("add a few more lines so it shows as modified");
-console.error("fun");
+console.error("fun!");
 console.error("yet again");
 console.log("how do you solve a problem like?");
 
