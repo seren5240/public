51e7210aa3a59d286aa58cad2fc47ca60f5598be
Seren Kwok <86184664+seren5240@users.noreply.github.com>
2023-11-30 15:45:25 +0000

    Update another.ts

--- another.ts
+++ another.ts
@@ -1,6 +1,6 @@
 console.error("add a few more lines so it shows as modified");
 console.error("fun!");
 console.error("yet again");
-console.log("how do you solve a problem like?");
+console.log("make another change here");
 
 export const unused = 5;
